f33b7321c8d2c63d1a153f2fb043f5effaa05afe
asiutra <adamsiutra@onet.pl>
2020-09-20 20:06:43 +0200

    Prepare service (CRUD Operation)

---
+++ RESTfulAPI_CarService/RESTfulAPI_CarService/Models/CarService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTfulAPI_CarService.Models
+{
+    public class CarService
+    {
+        public int Id { get; set; }
+        public string WhatService { get; set; }
+        public string When { get; set; }
+        public string Price { get; set; }
+    }
+}
---
+++ RESTfulAPI_CarService/RESTfulAPI_CarService/Service/Repository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTfulAPI_CarService.Models;
+
+namespace RESTfulAPI_CarService.Service
+{
+    public class Repository : IRepository
+    {
+        private Dictionary<int, CarService> items;
+
+        public Repository()
+        {
+            //Prepare some data on start.
+            items = new Dictionary<int, CarService>();
+            new List<CarService>
+            {
+                new CarService { Id = 1, Price = "150", WhatService = "Lacznik stabilizatora", When = "12.09.2020"},
+                new CarService { Id = 1, Price = "150", WhatService = "Lacznik stabilizatora", When = "12.09.2020"},
+                new CarService { Id = 1, Price = "150", WhatService = "Lacznik stabilizatora", When = "12.09.2020"}
+            }.ForEach(cs => AddCarService(cs));
+        }
+
+        public IEnumerable<CarService> CarServices => items.Values;
+        public CarService this[int id] => items.ContainsKey(id) ? items[id] : null;
+
+        public CarService AddCarService(CarService carService)
+        {
+            if(carService.Id == 0)
+            {
+                int key = items.Count;
+                while (items.ContainsKey(key)) { key++; }
+                carService.Id = key;
+            }
+
+            items[carService.Id] = carService;
+            return carService;
+        }
+
+        public CarService UpdateCarService(CarService carService) => AddCarService(carService);
+
+        public void DeleteCarService(int id) => items.Remove(id);
+    }
+}
